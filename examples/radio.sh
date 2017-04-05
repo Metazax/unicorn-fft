fa157e5d5188960bbc356f83aea39f99d940b20a
daniel-j <daniel.j.hede@gmail.com>
2017-04-05 16:28:46 +0200

    quick fix broken stream

--- examples/radio.sh
+++ examples/radio.sh
@@ -2,5 +2,5 @@
 cd "$(dirname "$0")"
 
 # this example will visualize a web radio
-mpg123 -s --no-seekbuffer -m -r 44100 http://icecast.djazz.se:8000/radio | sudo ../unicorn-fft
+mpg123 -s --no-seekbuffer -m -r 44100 http://icecast.djazz.se:8000/gnr | sudo ../unicorn-fft
 
